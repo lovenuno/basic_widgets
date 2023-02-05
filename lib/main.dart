49d5e4648b17853d437555fd52ab8ca9ccdc1f61
love NUNO <nuno08@gmail.com>
2023-02-05 21:11:20 +0900

    Add button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:basic_widgets/widgets/text_button_widget.dart';
+import 'package:basic_widgets/widgets/outline_button.dart';
+import 'package:basic_widgets/widgets/elevate_button.dart';
 
 void main(){
   runApp(MyApp());
@@ -13,7 +15,9 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: TextButtonWidgetExample(),
+          //child: TextButtonWidgetExample(),
+          // child: OutlinedButtonWidgetExample(),
+          child: ElevatedButtonWidgetExample(),
         ),
       ),
     );
---
+++ lib/widgets/elevate_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ElevatedButtonWidgetExample extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+            ),
+            child: Text('Elevated button'),
+          ),
+        ),
+      ),
+    );
+  }
+}
